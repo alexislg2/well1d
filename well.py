6bb0f25bb02821dabe4f8893ffc5dfb6264a20d6
Alexis LE GOFF <alexis@asweshare.com>
2024-07-03 07:55:04 +0200

    rm comments

--- well.py
+++ well.py
@@ -1,24 +1,15 @@
 import time
 import serial
-# Define the serial port and baud rate.
-
 
 
 def get_depth_mm():
-    # Define the serial port and baud rate.
     port = '/dev/ttyACM0'
     baudrate = 9600
-    # Create a serial connection.
     ser = serial.Serial(port, baudrate, timeout=1)
-    # Convert the hexadecimal command to bytes.
     command = bytes.fromhex('01 03 00 04 00 01 C5 CB')
-    # Send the command to the serial port.
     ser.write(command)
-    # Wait for a response (adjust the sleep time if necessary).
     time.sleep(.1)
-    # Read the response from the serial port.
-    response = ser.read(7)  # Read 8 bytes (adjust the number of bytes to read according to your expected response length)
-    # Close the serial connection.
+    response = ser.read(7)
     ser.close()
     if response == b'':
         return None
