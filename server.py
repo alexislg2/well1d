80568f8e19898279de1049b7013a6450eba4430d
Alexis LE GOFF <alexis@asweshare.com>
2024-07-16 13:57:38 +0200

    hauteur et litres

--- server.py
+++ server.py
@@ -11,6 +11,8 @@ from datetime import datetime
 app = Flask(__name__)
 
 DATABASE = 'well.db'
+WELL_RADIUS = .78
+WELL_HEIGHT = 3155
 
 def create_database():
     if not os.path.exists(DATABASE):
@@ -51,26 +53,51 @@ def data():
     return jsonify(data)
 
 def mm_to_liters(mm):
-    WELL_RADIUS = .78
     return math.pow(WELL_RADIUS, 2) * math.pi * mm
 
 @app.route('/')
 def plot():
     data = get_data()
     timestamps = [row[0] for row in data]
-    heights = [mm_to_liters(row[1]) for row in data]
+    heights = [row[1] for row in data]
+    volumes = [mm_to_liters(h) for h in heights]
+    hover_texts = [f"Hauteur: {height} mm<br>Volume estimé: {liter:.2f} L" for height, liter in zip(heights, volumes)]
 
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=timestamps, y=heights, mode='lines', name='Volume d\'eau'))
-    fig.update_layout(title='Volume d\'eau dans le puits',
-                      xaxis_title='Temps',
-                      yaxis_title='Volume d\'eau (litres)',
-                      xaxis=dict(tickangle=-45),
-                      height=800)
-
+    fig.add_trace(go.Scatter(x=timestamps, y=volumes, mode='lines', name='Volume d\'eau', text=hover_texts, hoverinfo='text'))
+    fig.add_shape(
+        type="line",
+        x0=timestamps[0], x1=timestamps[-1],
+        y0=mm_to_liters(WELL_HEIGHT), y1=mm_to_liters(WELL_HEIGHT),
+        line=dict(
+            color="Red",
+            width=2,
+            dash="dashdot",
+        ),
+        name="Hauteur max du puits"
+    )
+    fig.add_annotation(
+        x=timestamps[len(timestamps)//8],
+        y=mm_to_liters(WELL_HEIGHT),
+        text="Hauteur max du puits",
+        showarrow=False,
+        yshift=10,
+        font=dict(
+            color="Red",
+            size=12
+        )
+    )
+    fig.update_layout(
+        title="Volume d'eau dans le puits",
+        xaxis_title='Temps',
+        yaxis_title='Volume estime (litres)',
+        xaxis=dict(tickangle=-45),
+        height=800
+    )
+    
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return render_template('plot.html', graphJSON=graphJSON)
 
 if __name__ == '__main__':
     create_database()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
