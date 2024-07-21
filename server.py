cb7f0dcda4412e042a6f983739d3432e6b9a8576
Alexis LE GOFF <alexis@asweshare.com>
2024-07-21 09:58:47 +0200

    boutons par périodes

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import plotly.graph_objs as go
 import json
 import math
 import pytz
-from datetime import datetime
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -31,14 +31,23 @@ def insert_data(timestamp, height):
     conn.commit()
     conn.close()
 
-def get_n_minute_averages(n):
+def get_n_minute_averages(n, from_timestamp=None, to_timestamp=None):
     conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
-    c.execute(f"""
-        SELECT (timestamp - (timestamp % {60 * n})) as interval, AVG(height_mm) as avg_height
+
+    query = """
+        SELECT (timestamp - (timestamp % (? * 60))) as interval, AVG(height_mm) as avg_height
         FROM water_height
-        GROUP BY interval
-    """)
+    """
+    params = [n]
+
+    if from_timestamp is not None and to_timestamp is not None:
+        query += " WHERE timestamp BETWEEN ? AND ?"
+        params.extend([from_timestamp, to_timestamp])
+
+    query += " GROUP BY interval"
+
+    c.execute(query, params)
     data = c.fetchall()
     conn.close()
     return data
@@ -69,7 +78,21 @@ def mm_to_liters(mm):
 
 @app.route('/')
 def plot():
-    data = get_n_minute_averages(5)
+    from_timestamp = request.args.get('from')
+    to_timestamp = request.args.get('to')
+    n = int(request.args.get('n', 5))  # Default to 1 minute aggregation
+
+    if from_timestamp and to_timestamp:
+        from_timestamp_dt = datetime.strptime(from_timestamp, '%Y-%m-%d %H:%M:%S')
+        to_timestamp_dt = datetime.strptime(to_timestamp, '%Y-%m-%d %H:%M:%S')
+    else:
+        to_timestamp_dt = datetime.now()
+        from_timestamp_dt = to_timestamp_dt - timedelta(days=7)
+
+    from_timestamp_unix = int(from_timestamp_dt.timestamp())
+    to_timestamp_unix = int(to_timestamp_dt.timestamp())
+
+    data = get_n_minute_averages(n, from_timestamp_unix, to_timestamp_unix)
     timestamps = [datetime.fromtimestamp(row[0], pytz.timezone("Europe/Paris")).strftime('%Y-%m-%d %H:%M:%S') for row in data]
     timestamps_human_readable = [datetime.fromtimestamp(row[0], pytz.timezone("Europe/Paris")).strftime('%d/%m/%Y %H:%M') for row in data]
     heights = [row[1] for row in data]
@@ -109,8 +132,16 @@ def plot():
     )
     
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template('plot.html', graphJSON=graphJSON)
+    now = datetime.now()
 
+    period_duration = to_timestamp_dt - from_timestamp_dt
+    prev_from = (from_timestamp_dt - period_duration).strftime('%Y-%m-%d %H:%M:%S')
+    prev_to = (to_timestamp_dt - period_duration).strftime('%Y-%m-%d %H:%M:%S')
+    next_from = (from_timestamp_dt + period_duration).strftime('%Y-%m-%d %H:%M:%S')
+    next_to = (to_timestamp_dt + period_duration).strftime('%Y-%m-%d %H:%M:%S')
+
+    return render_template('plot.html', graphJSON=graphJSON, now=datetime.now(), timedelta=timedelta,
+                           prev_from=prev_from, prev_to=prev_to, next_from=next_from, next_to=next_to, n=n)
 if __name__ == '__main__':
     create_database()
     app.run(host='0.0.0.0', port=5000)
